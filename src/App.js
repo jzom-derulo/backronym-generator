42f791f251fd2b03360f87ac2f59ced7b40abd50
Olivia Heulitt <oheulitt120@gmail.com>
2021-05-23 10:23:12 -0700

    trying this again with deleting the importing of the saved backronym component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import firebase from './config/firebase.js';
 import UserInputForm from './component/UserInputForm.js';
-import SavedBackronyms from "./component/SavedBackronyms.js";
+
 // import hooks
 // create firebase database and import 
 
@@ -109,7 +109,7 @@ function App() {
       <UserInputForm handleClick={handleClick} />
       <button onClick={changeLetters}>change the letters</button>
 
-      <SavedBackronyms/>
+      
     </>
   );
 }
